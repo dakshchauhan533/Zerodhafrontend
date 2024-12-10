b0f430eb372fc081203df5352ca7315440970bfe
daksh chauhan <chauhandaksh60@gmail.com>
2024-12-10 20:46:30 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-   <div><h1>Hello</h1></div>
+   <div><h1>boty</h1></div>
   </React.StrictMode>
 );
 
